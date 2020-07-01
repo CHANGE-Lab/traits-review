1484e9f2dea9adfde06db96b5404bf9b749f8032
Cole B. Brookson <brookson@ualberta.ca>
2020-07-01 02:26:51 -0600

    add proper pipeline for simper data output

---
+++ Code/Cole-Code/Simper/simper_models.R
@@ -0,0 +1,110 @@
+########## 
+##########
+# This code contains the multivariate modeling component of the analysis presented in
+# in Green et al. (2020) 
+# A review on the use of traits in ecological research
+##########
+##########
+# AUTHOR: Cole B. Brookson
+# DATE OF CREATION: 2020-06-30
+##########
+##########
+
+library(knitr)
+library(tidyverse)
+library(vegan)
+library(viridis)
+library(PNWColors)
+library(mvabund)
+library(reshape2)
+library(here)
+
+################################## Simper for the different groups
+
+#load data
+original_traits = read_csv(here('./Data/Cole-Output-Data(readyforanalysis)/original_traits_dummy.csv'))
+oiriginal_dummy = original_traits[,1:8]
+oiriginal_categorical = original_traits[,9:ncol(original_traits)]
+
+######################## Ecosystem
+
+simper_original_ecos = simper(oiriginal_dummy, oiriginal_categorical$Ecosystem, permutations = 1000)
+
+summary_ecos = summary(simper_original_ecos)
+
+write_csv(simper_original_ecos$Freshwater_Terrestrial, 
+          here("./Data/Cole-Output-Simper/Original-Classification/simper_original_ecos_Freshwater_Terrestrial.csv"))
+write_csv(simper_original_ecos$Freshwater_Marine, 
+          here("./Data/Cole-Output-Simper/Original-Classification/simper_original_ecos_Freshwater_Marine.csv"))
+write_csv(simper_original_ecos$Freshwater_Broad, 
+          here("./Data/Cole-Output-Simper/Original-Classification/simper_original_ecos_Freshwater_Broad.csv"))
+write_csv(simper_original_ecos$Terrestrial_Marine, 
+          here("./Data/Cole-Output-Simper/Original-Classification/simper_original_ecos_Terrestrial_Marine.csv"))
+write_csv(simper_original_ecos$Terrestrial_Broad, 
+          here("./Data/Cole-Output-Simper/Original-Classification/simper_original_ecos_Terrestrial_Broad.csv"))
+write_csv(simper_original_ecos$Marine_Broad, 
+          here("./Data/Cole-Output-Simper/Original-Classification/simper_original_ecos_Marine_Broad.csv"))
+
+simper_original_filter = simper(oiriginal_dummy, oiriginal_categorical$filter, permutations = 1000)
+
+######################## Filter
+
+summary_filter = summary(simper_original_filter)
+
+write_csv(simper_original_filter$Ecological_Fundamental, 
+          here("./Data/Cole-Output-Simper/Original-Classification/simper_original_filter_Ecological_Fundamental.csv"))
+write_csv(simper_original_filter$Ecological_Physical, 
+          here("./Data/Cole-Output-Simper/Original-Classification/simper_original_filter_Ecological_Physical.csv"))
+write_csv(simper_original_filter$Ecological_Trophic, 
+          here("./Data/Cole-Output-Simper/Original-Classification/simper_original_filter_Ecological_Trophic.csv"))
+write_csv(simper_original_filter$Fundamental_Physical, 
+          here("./Data/Cole-Output-Simper/Original-Classification/simper_original_filter_Fundamental_Physical.csv"))
+write_csv(simper_original_filter$Fundamental_Trophic, 
+          here("./Data/Cole-Output-Simper/Original-Classification/simper_original_filter_Fundamental_Trophic.csv"))
+write_csv(simper_original_filter$Physical_Trophic, 
+          here("./Data/Cole-Output-Simper/Original-Classification/simper_original_filter_Physical_Trophic.csv"))
+
+######################## Global Change
+
+simper_original_gc = simper(oiriginal_dummy, oiriginal_categorical$GlobalChange, permutations = 1000)
+
+summary_gc = summary(simper_original_gc)
+
+write_csv(simper_original_gc$yes_no, 
+          here("./Data/Cole-Output-Simper/Original-Classification/simper_original_gc_yes_no.csv"))
+
+######################## Type of Study
+
+simper_original_tos = simper(oiriginal_dummy, oiriginal_categorical$TOS, permutations = 1000)
+
+summary_tos = summary(simper_original_tos)
+
+write_csv(simper_original_tos$Observational_Experiment, 
+          here("./Data/Cole-Output-Simper/Original-Classification/simper_original_tos_Observational_Experiment.csv"))
+write_csv(simper_original_tos$Observational_Review, 
+          here("./Data/Cole-Output-Simper/Original-Classification/simper_original_tos_Observational_Review.csv"))
+write_csv(simper_original_tos$Observational_Metanalysis, 
+          here("./Data/Cole-Output-Simper/Original-Classification/simper_original_tos_Observational_Metanalysis.csv"))
+write_csv(simper_original_tos$Observational_TModel, 
+          here("./Data/Cole-Output-Simper/Original-Classification/simper_original_tos_Observational_TModel.csv"))
+write_csv(simper_original_tos$Experiment_Review, 
+          here("./Data/Cole-Output-Simper/Original-Classification/simper_original_tos_Experiment_Review.csv"))
+write_csv(simper_original_tos$Experiment_Metanalysis, 
+          here("./Data/Cole-Output-Simper/Original-Classification/simper_original_tos_Experiment_Metanalysis.csv"))
+write_csv(simper_original_tos$Experiment_TModel, 
+          here("./Data/Cole-Output-Simper/Original-Classification/simper_original_tos_Experiment_TModel.csv"))
+write_csv(simper_original_tos$Review_Metanalysis, 
+          here("./Data/Cole-Output-Simper/Original-Classification/simper_original_tos_Review_Metanalysis.csv"))
+write_csv(simper_original_tos$Review_TModel, 
+          here("./Data/Cole-Output-Simper/Original-Classification/simper_original_tos_Review_TModel.csv"))
+write_csv(simper_original_tos$Metanalysis_TModel, 
+          here("./Data/Cole-Output-Simper/Original-Classification/simper_original_tos_Metanalysis_TModel.csv"))
+
+######################## Prediction
+
+simper_original_predict = simper(oiriginal_dummy, oiriginal_categorical$Forecasting, permutations = 1000)
+
+write_csv(simper_original_predict$`0_1`, 
+          here("./Data/Cole-Output-Simper/Original-Classification/simper_original_PREDICT.csv"))
+
+
--- Code/Cole-Code/primary_abund_multivar_models.R
+++ Code/Cole-Code/primary_abund_multivar_models.R
@@ -38,20 +38,20 @@ set.seed(00001)
 # stressplot(primary_abundance_ord_k3)
 # primary_abundance_ord_k3$stress
 
-primary_abundance_ord_k2 = metaMDS(primary_abundance_species,
+primary_abundance_ord_k3 = metaMDS(primary_abundance_species,
                                      #distance = 'bray',
-                                     trymax = 100,
-                                     k = 2)
-plot(primary_abundance_ord_k2)
-summary(primary_abundance_ord_k2)
-stressplot(primary_abundance_ord_k2)
-primary_abundance_ord_k2$stress
+                                     trymax = 1000,
+                                     k = 3)
+plot(primary_abundance_ord_k3)
+summary(primary_abundance_ord_k3)
+stressplot(primary_abundance_ord_k3)
+primary_abundance_ord_k3$stress
 
 #extract scores -- looks like k = 3 is the way to go 
-primary_abundance_ord_k2_scores = data.frame(scores(primary_abundance_ord_k2))
-primary_abundance_ord_k2_scores = cbind(primary_abundance_ord_k2_scores,
+primary_abundance_ord_k3_scores = data.frame(scores(primary_abundance_ord_k3))
+primary_abundance_ord_k3_scores = cbind(primary_abundance_ord_k3_scores,
                                         primary_abundance_sites)
-rownames(primary_abundance_ord_k2_scores) = primary_abundance_ord_k2_scores$DOI
+rownames(primary_abundance_ord_k3_scores) = primary_abundance_ord_k3_scores$DOI
 
 
 ###################################### make all NMDS plots
@@ -59,33 +59,33 @@ rownames(primary_abundance_ord_k2_scores) = primary_abundance_ord_k2_scores$DOI
 ######################## Ecosystem
 
 #split
-primary_abundance_ord_k2_scores$Ecosystem = 
-  as.character(primary_abundance_ord_k2_scores$Ecosystem)
+primary_abundance_ord_k3_scores$Ecosystem = 
+  as.character(primary_abundance_ord_k3_scores$Ecosystem)
 
-primary_abundance_ord_k2_scores_split_ecosystem = 
-  split(primary_abundance_ord_k2_scores, 
-        primary_abundance_ord_k2_scores$Ecosystem)
+primary_abundance_ord_k3_scores_split_ecosystem = 
+  split(primary_abundance_ord_k3_scores, 
+        primary_abundance_ord_k3_scores$Ecosystem)
 
 #lapply with chull
-primary_abundance_ord_k2_scores_applied_ecosystem = 
-  lapply(primary_abundance_ord_k2_scores_split_ecosystem, 
+primary_abundance_ord_k3_scores_applied_ecosystem = 
+  lapply(primary_abundance_ord_k3_scores_split_ecosystem, 
   function(df){
   df[chull(df), ]
 })
 
 #combine things
-primary_abundance_ord_k2_scores_combined_ecosystem = 
+primary_abundance_ord_k3_scores_combined_ecosystem = 
   do.call(rbind, 
-          primary_abundance_ord_k2_scores_applied_ecosystem)
+          primary_abundance_ord_k3_scores_applied_ecosystem)
 
 #plot things 
-primary_abundance_ord_k2_plot_ecosystem = ggplot() + 
-  geom_polygon(data=primary_abundance_ord_k2_scores_combined_ecosystem,
+primary_abundance_ord_k3_plot_ecosystem = ggplot() + 
+  geom_polygon(data=primary_abundance_ord_k3_scores_combined_ecosystem,
                aes(x=NMDS1,y=NMDS2, 
                    fill=fct_rev(Ecosystem),
                    group=fct_rev(Ecosystem)), 
                alpha=0.30) + # add the convex hulls
-  geom_point(data=primary_abundance_ord_k2_scores_combined_ecosystem,
+  geom_point(data=primary_abundance_ord_k3_scores_combined_ecosystem,
              aes(x=NMDS1,y=NMDS2, 
                  colour = fct_rev(Ecosystem)), 
              size=2) + # add the point markers
@@ -107,38 +107,38 @@ primary_abundance_ord_k2_plot_ecosystem = ggplot() +
   scale_colour_viridis(discrete = TRUE, option="magma",
                        begin = 0.8, end = 0.2, name = 'Ecosystem')
 
-primary_abundance_ord_k2_plot_ecosystem
+primary_abundance_ord_k3_plot_ecosystem
 
 ######################## Global Change
 
 #split
-primary_abundance_ord_k2_scores$GlobalChangeCat = 
-  as.character(primary_abundance_ord_k2_scores$GlobalChangeCat)
+primary_abundance_ord_k3_scores$GlobalChangeCat = 
+  as.character(primary_abundance_ord_k3_scores$GlobalChangeCat)
 
-primary_abundance_ord_k2_scores_split_change = 
-  split(primary_abundance_ord_k2_scores, 
-        primary_abundance_ord_k2_scores$GlobalChangeCat)
+primary_abundance_ord_k3_scores_split_change = 
+  split(primary_abundance_ord_k3_scores, 
+        primary_abundance_ord_k3_scores$GlobalChangeCat)
 
 #lapply with chull
-primary_abundance_ord_k2_scores_applied_change = 
-  lapply(primary_abundance_ord_k2_scores_split_change, 
+primary_abundance_ord_k3_scores_applied_change = 
+  lapply(primary_abundance_ord_k3_scores_split_change, 
          function(df){
            df[chull(df), ]
          })
 
 #combine things
-primary_abundance_ord_k2_scores_combined_change = 
+primary_abundance_ord_k3_scores_combined_change = 
   do.call(rbind, 
-          primary_abundance_ord_k2_scores_applied_change)
+          primary_abundance_ord_k3_scores_applied_change)
 
 #plot things 
-primary_abundance_ord_k2_plot_change = ggplot() + 
-  geom_polygon(data=primary_abundance_ord_k2_scores_combined_change,
+primary_abundance_ord_k3_plot_change = ggplot() + 
+  geom_polygon(data=primary_abundance_ord_k3_scores_combined_change,
                aes(x=NMDS1,y=NMDS2, 
                    fill=fct_rev(GlobalChangeCat),
                    group=fct_rev(GlobalChangeCat)), 
                alpha=0.30) + # add the convex hulls
-  geom_point(data=primary_abundance_ord_k2_scores_combined_change,
+  geom_point(data=primary_abundance_ord_k3_scores_combined_change,
              aes(x=NMDS1,y=NMDS2, 
                  colour = fct_rev(GlobalChangeCat)), 
              size=2) + # add the point markers
@@ -160,38 +160,38 @@ primary_abundance_ord_k2_plot_change = ggplot() +
   scale_colour_viridis(discrete = TRUE, option="magma",
                        begin = 0.8, end = 0.2, name = 'Global Change')
 
-primary_abundance_ord_k2_plot_change
+primary_abundance_ord_k3_plot_change
 
 ######################## Taxonomic Specific
 
 #split
-primary_abundance_ord_k2_scores$Taxonomic = 
-  as.character(primary_abundance_ord_k2_scores$Taxonomic)
+primary_abundance_ord_k3_scores$Taxonomic = 
+  as.character(primary_abundance_ord_k3_scores$Taxonomic)
 
-primary_abundance_ord_k2_scores_split_taxonomic = 
-  split(primary_abundance_ord_k2_scores, 
-        primary_abundance_ord_k2_scores$Taxonomic)
+primary_abundance_ord_k3_scores_split_taxonomic = 
+  split(primary_abundance_ord_k3_scores, 
+        primary_abundance_ord_k3_scores$Taxonomic)
 
 #lapply with chull
-primary_abundance_ord_k2_scores_applied_taxonomic = 
-  lapply(primary_abundance_ord_k2_scores_split_taxonomic, 
+primary_abundance_ord_k3_scores_applied_taxonomic = 
+  lapply(primary_abundance_ord_k3_scores_split_taxonomic, 
          function(df){
            df[chull(df), ]
          })
 
 #combine things
-primary_abundance_ord_k2_scores_combined_taxonomic = 
+primary_abundance_ord_k3_scores_combined_taxonomic = 
   do.call(rbind, 
-          primary_abundance_ord_k2_scores_applied_taxonomic)
+          primary_abundance_ord_k3_scores_applied_taxonomic)
 
 #plot things 
-primary_abundance_ord_k2_plot_taxonomic = ggplot() + 
-  geom_polygon(data=primary_abundance_ord_k2_scores_combined_taxonomic,
+primary_abundance_ord_k3_plot_taxonomic = ggplot() + 
+  geom_polygon(data=primary_abundance_ord_k3_scores_combined_taxonomic,
                aes(x=NMDS1,y=NMDS2, 
                    fill=fct_rev(Taxonomic),
                    group=fct_rev(Taxonomic)), 
                alpha=0.30) + # add the convex hulls
-  geom_point(data=primary_abundance_ord_k2_scores_combined_taxonomic,
+  geom_point(data=primary_abundance_ord_k3_scores_combined_taxonomic,
              aes(x=NMDS1,y=NMDS2, 
                  colour = fct_rev(Taxonomic)), 
              size=2) + # add the point markers
@@ -213,50 +213,50 @@ primary_abundance_ord_k2_plot_taxonomic = ggplot() +
   scale_colour_viridis(discrete = TRUE, option="magma",
                        begin = 0.8, end = 0.2, name = 'Taxonomic Group')
 
-primary_abundance_ord_k2_plot_taxonomic
+primary_abundance_ord_k3_plot_taxonomic
 
 ######################## Taxonomic Group
 
 #add in taxonomic group
-primary_abundance_ord_k2_scores = primary_abundance_ord_k2_scores %>% 
-  mutate(TaxGroup = ifelse(primary_abundance_ord_k2_scores$Taxonomic == 'Plants', 'Plants', 
-                ifelse(primary_abundance_ord_k2_scores$Taxonomic %in% 
+primary_abundance_ord_k3_scores = primary_abundance_ord_k3_scores %>% 
+  mutate(TaxGroup = ifelse(primary_abundance_ord_k3_scores$Taxonomic == 'Plants', 'Plants', 
+                ifelse(primary_abundance_ord_k3_scores$Taxonomic %in% 
                          c('Mammals', 'Herpetofauna', 'Fish', 'Birds'), 'Vertebrates',
-                       ifelse(primary_abundance_ord_k2_scores$Taxonomic %in%
+                       ifelse(primary_abundance_ord_k3_scores$Taxonomic %in%
                                 c('Plankton', 'Insects'), 'Invertebrates',
-                              ifelse(primary_abundance_ord_k2_scores$Taxonomic == 
+                              ifelse(primary_abundance_ord_k3_scores$Taxonomic == 
                                        'Other','Other',
-                                     ifelse(primary_abundance_ord_k2_scores$Taxonomic == 'Multiple',
+                                     ifelse(primary_abundance_ord_k3_scores$Taxonomic == 'Multiple',
                                             'Multiple', NA))))))
 
 #split
-primary_abundance_ord_k2_scores$TaxGroup = 
-  as.character(primary_abundance_ord_k2_scores$TaxGroup)
+primary_abundance_ord_k3_scores$TaxGroup = 
+  as.character(primary_abundance_ord_k3_scores$TaxGroup)
 
-primary_abundance_ord_k2_scores_split_taxgroup = 
-  split(primary_abundance_ord_k2_scores, 
-        primary_abundance_ord_k2_scores$TaxGroup)
+primary_abundance_ord_k3_scores_split_taxgroup = 
+  split(primary_abundance_ord_k3_scores, 
+        primary_abundance_ord_k3_scores$TaxGroup)
 
 #lapply with chull
-primary_abundance_ord_k2_scores_applied_taxgroup = 
-  lapply(primary_abundance_ord_k2_scores_split_taxgroup, 
+primary_abundance_ord_k3_scores_applied_taxgroup = 
+  lapply(primary_abundance_ord_k3_scores_split_taxgroup, 
          function(df){
            df[chull(df), ]
          })
 
 #combine things
-primary_abundance_ord_k2_scores_combined_taxgroup = 
+primary_abundance_ord_k3_scores_combined_taxgroup = 
   do.call(rbind, 
-          primary_abundance_ord_k2_scores_applied_taxgroup)
+          primary_abundance_ord_k3_scores_applied_taxgroup)
 
 #plot things 
-primary_abundance_ord_k2_plot_taxgroup = ggplot() + 
-  geom_polygon(data=primary_abundance_ord_k2_scores_combined_taxgroup,
+primary_abundance_ord_k3_plot_taxgroup = ggplot() + 
+  geom_polygon(data=primary_abundance_ord_k3_scores_combined_taxgroup,
                aes(x=NMDS1,y=NMDS2, 
                    fill=fct_rev(TaxGroup),
                    group=fct_rev(TaxGroup)), 
                alpha=0.30) + # add the convex hulls
-  geom_point(data=primary_abundance_ord_k2_scores_combined_taxgroup,
+  geom_point(data=primary_abundance_ord_k3_scores_combined_taxgroup,
              aes(x=NMDS1,y=NMDS2, 
                  colour = fct_rev(TaxGroup)), 
              size=2) + # add the point markers
@@ -278,38 +278,38 @@ primary_abundance_ord_k2_plot_taxgroup = ggplot() +
   scale_colour_viridis(discrete = TRUE, option="magma",
                        begin = 0.8, end = 0.2, name = 'Taxonomic Group')
 
-primary_abundance_ord_k2_plot_taxgroup
+primary_abundance_ord_k3_plot_taxgroup
 
 ######################## Type of Study
 
 #split
-primary_abundance_ord_k2_scores$TOS = 
-  as.character(primary_abundance_ord_k2_scores$TOS)
+primary_abundance_ord_k3_scores$TOS = 
+  as.character(primary_abundance_ord_k3_scores$TOS)
 
-primary_abundance_ord_k2_scores_split_tos = 
-  split(primary_abundance_ord_k2_scores, 
-        primary_abundance_ord_k2_scores$TOS)
+primary_abundance_ord_k3_scores_split_tos = 
+  split(primary_abundance_ord_k3_scores, 
+        primary_abundance_ord_k3_scores$TOS)
 
 #lapply with chull
-primary_abundance_ord_k2_scores_applied_tos = 
-  lapply(primary_abundance_ord_k2_scores_split_tos, 
+primary_abundance_ord_k3_scores_applied_tos = 
+  lapply(primary_abundance_ord_k3_scores_split_tos, 
          function(df){
            df[chull(df), ]
          })
 
 #combine things
-primary_abundance_ord_k2_scores_combined_tos = 
+primary_abundance_ord_k3_scores_combined_tos = 
   do.call(rbind, 
-          primary_abundance_ord_k2_scores_applied_tos)
+          primary_abundance_ord_k3_scores_applied_tos)
 
 #plot things 
-primary_abundance_ord_k2_plot_tos = ggplot() + 
-  geom_polygon(data=primary_abundance_ord_k2_scores_combined_tos,
+primary_abundance_ord_k3_plot_tos = ggplot() + 
+  geom_polygon(data=primary_abundance_ord_k3_scores_combined_tos,
                aes(x=NMDS1,y=NMDS2, 
                    fill=fct_rev(TOS),
                    group=fct_rev(TOS)), 
                alpha=0.30) + # add the convex hulls
-  geom_point(data=primary_abundance_ord_k2_scores_combined_tos,
+  geom_point(data=primary_abundance_ord_k3_scores_combined_tos,
              aes(x=NMDS1,y=NMDS2, 
                  colour = fct_rev(TOS)), 
              size=2) + # add the point markers
@@ -331,7 +331,7 @@ primary_abundance_ord_k2_plot_tos = ggplot() +
   scale_colour_viridis(discrete = TRUE, option="magma",
                        begin = 0.8, end = 0.2, name = 'Type of Study')
 
-primary_abundance_ord_k2_plot_tos
+
 ## Filter
 
 
