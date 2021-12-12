27c4d515e836774b4d55f8f50d6f51e1697cdfc0
colebrookson <cole.brookson@gmail.com>
2021-12-12 16:08:23 -0500

    use the kable option

---
+++ src/09_match_up_dois.R
@@ -0,0 +1,57 @@
+########## 
+##########
+# This script matches the DOIs to Journal names for the papers included  
+# in Green et al. (2021) 
+# A review on the use of traits in ecological research
+##########
+##########
+# AUTHOR: Cole B. Brookson
+# DATE OF CREATION: 2021-12-12
+##########
+##########
+
+# set-up =======================================================================
+
+library(tidyverse)
+library(here)
+library(knitr)
+library(kableExtra)
+library(magick)
+library(webshot)
+install.packages("magick")
+install.packages("webshot")
+webshot::install_phantomjs()
+
+unique_data = read_csv(here('./data/unprocessed-data/unique_papers.csv')) %>% 
+  select(Year, Journal, DOI, `Relevant to Study`) %>% 
+  rename(Relevance = `Relevant to Study`) 
+unique_data$Relevance = as.factor(unique_data$Relevance)
+unique(unique_data$Relevance)
+unique_data = unique_data %>% 
+  filter(Relevance %in% c("Y", "y")) %>% 
+  select(-c(Year, Relevance))
+
+new_data = read_csv(
+  here("./data/unprocessed-data/study_classification_FuncBiogeog_to_append.csv")) %>% 
+  select(PY, SO, DOI, Relevance) %>% 
+  rename(Journal = SO, Year = PY) %>% 
+  filter(Relevance == 1) %>% 
+  select(-c(Year, Relevance))
+
+# join data ====================================================================
+
+all_data = rbind(new_data, unique_data)
+
+unique_journals = all_data %>% 
+  group_by(Journal) %>% 
+  summarize(`Number of Relevant Papers` = n())
+
+table = unique_journals[order(
+  unique_journals$`Number of Relevant Papers`, decreasing = TRUE),]
+
+write.table(table, here("./output-tables/journals_table.txt"))
+kable(table, "latex", booktabs = T) %>%
+  kable_styling(latex_options = c("striped", "scale_down")) %>% 
+  as_image
+  #save_kable(here("./output-tables/journals_table.png"))
+
