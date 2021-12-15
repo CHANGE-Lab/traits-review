735b774e6db468b4eb353e2b9234308d16546e2f
colebrookson <cole.brookson@gmail.com>
2021-12-15 13:12:02 -0700

    switch rmd over to R

---
+++ src/11_primary_abundance_manyglm.R
@@ -0,0 +1,23 @@
+########## 
+##########
+# This code contains the multivariate modeling component of the analysis presented in
+# in Green et al. (2020) 
+# A review on the use of traits in ecological research
+##########
+##########
+# AUTHOR: Cole B. Brookson
+# DATE OF CREATION: 2021-12-15
+##########
+##########
+
+# set-up =======================================================================
+
+library(devtools)
+library(knitr)
+library(tidyverse)
+library(vegan)
+library(viridis)
+library(PNWColors)
+library(mvabund)
+library(reshape2)
+library(here)
